5c77b92a4eb621b79f00318d62a657b394cf8986
Yotam <yotamshavit@gmail.com>
2022-08-15 18:00:05 +0300

    Added first stage logic

--- main.py
+++ main.py
@@ -14,3 +14,22 @@
 # imports
 import arcpy
 import os
+
+###
+### stage 1 - getting user parameters and setting the script's environment
+###
+
+arcpy.env.overwriteOutput = True
+
+# Get all user parameteres
+path = r'C:\Users\user\Desktop\PythonForUni\Final_project\testing_parent' #arcpy.GetParameterAsText(0) # get parent dir
+# try:
+#     work_area_parameter = arcpy.GetParameterAsText(1) # Polygonal layer 
+#     p1_desc = arcpy.Describe(work_area_parameter)
+#     if p1_desc.shapeType != "Polygon":
+#         raise TypeError
+# except TypeError:
+#     arcpy.AddError('A polygonal layer must be used for the work area parameter!')
+
+parent_dir = arcpy.env.workspace = path
+parent_desc = arcpy.Describe(parent_dir)
