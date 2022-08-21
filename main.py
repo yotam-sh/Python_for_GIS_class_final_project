b319becb5a1382414128f2d609786af791fcabd1
Yotam <yotamshavit@gmail.com>
2022-08-21 14:16:32 +0300

    New final comments

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
 """
 This script was written by Yotam Shavit
 
-The script was written for repetitive data conversion tasks for a friend.
+The script was requested by a friend of mine for repetitive data conversion tasks.
 The data input is always the same - municipal data that includes the following layers;
 roads-polyline # buildings-point # blocks-polygon
 
 What the script essentially does is as follows:
 1. Evaluates the data for incompleteness, meaning that any feature without complete attributes will not be valid
-2. Adding geometry attributes to the road layer, calculating road feature part lengths
-3. Creating a database and transferring all data to it, combining data from different folders into one workspace
-4. Renaming featureclass layers according to their containing folder and original name
-5. Cutting all data outside of an AOI polygon supplied by the user"""
+2. Adding geometry attributes
+3. Creating a database with templates from the existing layers
+4. Append valid data to it, combining data from different folders into one workspace
+5. Renaming featureclass layers according to their containing folder and original name
+6. Spatial-joinig buildings and blocks layers to populate a newly created field - 'new_number' """
 
 
 """ imports """
